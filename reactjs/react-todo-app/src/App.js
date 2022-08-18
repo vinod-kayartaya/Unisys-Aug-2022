bfb2d9ad6aca2d575e49637d0dbd3da2c3d83a5f
Vinod KK <vinod@vinod.co>
2022-08-18 09:36:10 +0530

    🇮🇳

--- reactjs/react-todo-app/src/App.js
+++ reactjs/react-todo-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import './App.css';
 import Header from './components/Header';
 import TodoList from './components/TodoList';
-import './App.css';
 import TodoForm from './components/TodoForm';
 
 export class App extends Component {
